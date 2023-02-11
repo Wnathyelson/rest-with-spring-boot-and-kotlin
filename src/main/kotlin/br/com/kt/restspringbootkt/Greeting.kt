65bef73ae8eeaac5a6f108b86b819c1188bbff3b
Wellington Nathyelson Nascimento de Lima <wellington.lima@compasso.com.br>
2023-02-11 18:42:17 -0300

    adding commit greeting

---
+++ src/main/kotlin/br/com/kt/restspringbootkt/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.kt.restspringbootkt
+
+class Greeting (val id: Long, val content: String)
---
+++ src/test/kotlin/br/com/kt/restspringbootkt/RestSpringBootKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.kt.restspringbootkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestSpringBootKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
