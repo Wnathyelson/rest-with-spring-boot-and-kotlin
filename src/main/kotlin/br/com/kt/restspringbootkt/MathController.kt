c5ed0bcd9be1340189b142f4e645e3f29fc64c23
Wellington Nathyelson Nascimento de Lima <wellington.lima@compasso.com.br>
2023-02-12 07:54:29 -0300

    adding more operations

--- src/main/kotlin/br/com/kt/restspringbootkt/MathController.kt
+++ src/main/kotlin/br/com/kt/restspringbootkt/MathController.kt
@@ -17,6 +17,49 @@ class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
     }
 
+    @RequestMapping(value = ["/sub/{numberOne}/{numberTwo}"])
+    fun sub(@PathVariable (value = "numberOne") numberOne: String?,
+            @PathVariable (value = "numberTwo") numberTwo: String?
+    ): Double{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        throw UnsupportedMathOperationException("Please, insert a numeric value!")
+        return convertToDouble(numberOne) - convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value=["/multi/{numberOne}/{numberTwo}"])
+    fun multi(@PathVariable (value = "numberOne") numberOne: String?,
+              @PathVariable (value = "numberTwo") numberTwo: String?
+    ): Double{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+        throw UnsupportedMathOperationException("Please, insert a numeric value!")
+        return  convertToDouble(numberOne) * convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value=["/divi/{numberOne}/{numberTwo}"])
+    fun divi(@PathVariable (value = "numberOne") numberOne: String?,
+              @PathVariable (value = "numberTwo") numberTwo: String?
+    ): Double{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please, insert a numeric value!")
+        return  convertToDouble(numberOne) / convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value=["/sqrt/{numberOne}"])
+    fun sqrt(@PathVariable (value = "numberOne") numberOne: String?
+    ): Double{
+        if(!isNumeric(numberOne))
+            throw UnsupportedMathOperationException("Please, insert a numeric value!")
+        return  convertToDouble(numberOne) * convertToDouble(numberOne)
+    }
+    @RequestMapping(value=["/med/{numberOne}/{numberTwo}"])
+    fun med(@PathVariable (value = "numberOne") numberOne: String?,
+             @PathVariable (value = "numberTwo") numberTwo: String?
+    ): Double{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please, insert a numeric value!")
+        return  (convertToDouble(numberOne) + convertToDouble(numberTwo))/2
+    }
+
     private fun convertToDouble(strNumber: String?): Double {
         if(strNumber.isNullOrBlank()) return 0.0
         val number = strNumber.replace(",".toRegex(),".")
